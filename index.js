47f0ba0159ef013bfddac7805b9cd89e85f695b3
Aaron <aahull08@gmail.com>
2022-07-15 09:35:11 -0500

    More Validation to the POST route

--- index.js
+++ index.js
@@ -29,13 +29,6 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  // const id = Number(request.params.id);
-  // const person = persons.find(person => person.id === id);
-  // if(!person){
-  //   response.status(402).end()
-  // } else {
-  //   response.send(person)
-  // }
   Person.findById(request.params.id)
     .then((person) => {
       if (person) {
@@ -58,7 +51,7 @@ app.delete('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.post('/api/persons/', (request, response) => {
+app.post('/api/persons/', (request, response, next) => {
   const body = request.body;
 
   if (!body.name) {
@@ -75,6 +68,7 @@ app.post('/api/persons/', (request, response) => {
   person.save().then(savedPerson => {
     response.json(savedPerson)
   })
+  .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response) => {
@@ -91,21 +85,22 @@ app.put('/api/persons/:id', (request, response) => {
     .catch(error => next(error))
 })
 
-// const errorHandler = (error, request, response, next) => {
-//   console.error(error);
+const errorHandler = (error, request, response, next) => {
+  console.error(error);
 
-//   if (error.name === 'CastError') {
-//     return response.status(400).send({ error: 'malformatted id' })
-//   } 
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError'){
+    return response.status(400).json({ error: error.message })
+  }
 
-//   next(error)
-// }
+  next(error)
+}
 
+app.use(errorHandler)
 
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
-
-// errorhandler middleware???
